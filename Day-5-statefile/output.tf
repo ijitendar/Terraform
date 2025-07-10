32fb4e7379b3effa424e168e3cf95861c7a3dc08
root <root@ip-172-31-0-11.ec2.internal>
2025-07-10 06:05:26 +0000

    make changes from remote

--- Day-5-statefile/output.tf
+++ Day-5-statefile/output.tf
@@ -1,3 +1,30 @@
-output "ip" {
-  value = aws_instance.web.public_ip
+# Output the EC2 instance public IP
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
 }
+
+# Output the EC2 instance ID
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.web.id
+}
+
+# Output the EC2 instance type
+output "instance_type" {
+  description = "The instance type of the EC2"
+  value       = aws_instance.web.instance_type
+}
+
+# Output the S3 bucket name
+output "s3_bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = aws_s3_bucket.name.id
+}
+
+# Optional: Output the S3 bucket ARN
+output "s3_bucket_arn" {
+  description = "The ARN of the S3 bucket"
+  value       = aws_s3_bucket.name.arn
+}
+
--- Day-5-statefile/terraform.tfvars
+++ Day-5-statefile/terraform.tfvars
@@ -1,3 +1,3 @@
 os   = "ami-05ffe3c48a9991133"  
 size = "t2.micro"
-name = "jeetbuckettobackupstatefile"
+name = "jeetbuckettobackupstatefiles"
